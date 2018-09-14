773d2175610ee322f1ff4c886f4a41ee53203469
CHANGJIAN ZHAO <changjianzhao@changjiandemac-mini.local>
2018-09-14 09:08:10 +0800

    扩展构造函数实现

--- DYZB/DYZB/Classes/Home/Controller/HomeViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/HomeViewController.swift
@@ -34,44 +34,19 @@ extension HomeViewController {
     
     //设置导航栏
     private func setupNavigationBar() {
-        //1 设置左侧的item
-        let btn = UIButton()
-        btn.setImage(UIImage(named: "logo"), for: .normal)
-        btn.sizeToFit()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
-
+        //1 设置UIBar左侧的item
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo", highImageName: "", size: CGSize(width: 0, height: 0))
+        
+        //2 设置UIBar右侧的按钮组
         let v_size = CGSize(width: 40, height: 40)
+        let historyItem = UIBarButtonItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: v_size)
+        let searchItem = UIBarButtonItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: v_size)
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: v_size)
+        /*
         let historyItem = UIBarButtonItem.createItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: v_size)
         let searchItem = UIBarButtonItem.createItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: v_size)
         let qrcodeItem = UIBarButtonItem.createItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: v_size)
-        
-        
-//        let size = CGSize(width: 40, height: 40)
-//        //2 设置右侧的item
-//        let historyBtn = UIButton()
-//        historyBtn.setImage(UIImage(named: "image_my_history"), for: .normal)
-//        historyBtn.setImage(UIImage(named: "Image_my_history_click"), for: .highlighted)
-//        historyBtn.sizeToFit()
-//        //historyBtn.frame = CGRect(origin: , size: size)
-//        let historyItem = UIBarButtonItem(customView: historyBtn)
-//
-//
-//        let searchBtn = UIButton()
-//        searchBtn.setImage(UIImage(named: "btn_search"), for: .normal)
-//        searchBtn.setImage(UIImage(named: "btn_search_clicked"), for: .highlighted)
-//        searchBtn.sizeToFit()
-//        //searchBtn.frame = CGRect(origin: CGPoint, size: size)
-//        let searchItem = UIBarButtonItem(customView: searchBtn)
-//
-//
-//        let qrcodeBtn = UIButton()
-//        qrcodeBtn.setImage(UIImage(named: "Image_scan"), for: .normal)
-//        qrcodeBtn.setImage(UIImage(named: "Image_scan_click"), for: .highlighted)
-//        qrcodeBtn.sizeToFit()
-//        //qrcodeBtn.frame = CGRect(origin: CGPoint, size: size)
-//        let qrcodeItem = UIBarButtonItem(customView: qrcodeBtn)
-//
-        
+        */
         navigationItem.rightBarButtonItems = [historyItem, searchItem, qrcodeItem]
         
     }
--- DYZB/DYZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
+++ DYZB/DYZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension UIBarButtonItem {
     
-    //扩展类方法
-    class func createItem(imageName : String, highImageName : String, size : CGSize) -> UIBarButtonItem {
+    //扩展类方法（swift开发中通常使用扩展构造函数的方式代替该类方法的实现方式）
+    /*class func createItem(imageName : String, highImageName : String, size : CGSize) -> UIBarButtonItem {
         let btn = UIButton()
         
         btn.setImage(UIImage(named: imageName), for: .normal)
@@ -20,5 +20,22 @@ extension UIBarButtonItem {
         
         return UIBarButtonItem(customView: btn)
     }
+    */
     
+    //构造函数（注意：只能使用便利构造函数：1>要以convenience开头 2>在构造函数中必须明确调用一个设计的构造函数（即使用self调用自定义的item））
+    convenience init(imageName : String, highImageName : String = "", size : CGSize = CGSize(width: 0, height: 0)) {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: imageName), for: .normal)
+        
+        if highImageName != "" {
+            btn.setImage(UIImage(named: highImageName), for: .normal)
+        }
+        if size == CGSize(width: 0, height: 0) {
+            btn.sizeToFit()
+        } else {
+            btn.frame = CGRect(origin: CGPoint(x: 0,y :0), size: size)
+        }
+        //创建UIBarButton
+        self.init(customView : btn) //调用自定义的item
+    }
 }
