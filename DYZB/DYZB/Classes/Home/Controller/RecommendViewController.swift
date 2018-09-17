512cc201d6521611ff54d15d327760296f7caaa0
CHANGJIAN ZHAO <changjianzhao@changjiandemac-mini.local>
2018-09-17 16:15:27 +0800

    界面布局全部完成：本次主要新增内容是实现自定义cell的布局功能实现（下一步：加载数据）

--- DYZB/DYZB/Classes/Home/Controller/RecommendViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/RecommendViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 //定义常量
 private let kItemMargin : CGFloat = 10
 private let kItemW = (kScreenW - 3 * kItemMargin) / 2
-private let kItemH = kItemW * 3 / 4
+//private let kItemH = kItemW * 3 / 4
+private let kNormalItemH = kItemW * 3 / 4
+private let kPrettyItemH = kItemW * 4 / 3
 private let kHeaderViewH : CGFloat = 50
 
 private let kNormalCellID = "kNormalCellID"
+private let kPrettyCellID = "kPrettyCellID"
 private let kHeaderViewID = "kHeaderViewID"
 
 class RecommendViewController: UIViewController {
@@ -23,7 +26,7 @@ class RecommendViewController: UIViewController {
     private lazy var collectionView : UICollectionView = {[unowned self] in
        //1 创建布局
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.itemSize = CGSize(width: kItemW, height: kItemW)
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMargin
         //设置组头(然后再在组头放一个view)第一步：设置大小
@@ -40,13 +43,18 @@ class RecommendViewController: UIViewController {
         collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         
         
-        //
+        //设置数据源
         collectionView.dataSource = self
+        //设置代理
+        collectionView.delegate = self //由于要实现定制化的cell的大小，所以需要实现特殊的协议：UICollectionViewDelegateFlowLayout 该协议还是继承至传统的代理协议UICollectionViewDelegate
+        
         
         //注册cell
         //collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
         //这里采用NIB自定义的cell
         collectionView.register(UINib(nibName: "CollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: kNormalCellID)
+        //这里采用NIB自定义的cell
+        collectionView.register(UINib(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: kPrettyCellID)
         //在每个组头中放置一个view（这里要先注册，在实现类方法）
         //第二步：注册
         //UICollectionElementKindSectionHeader-view要显示在组中的位置，这里表示显示在组的顶部
@@ -81,8 +89,8 @@ extension RecommendViewController {
     
 }
 
-// MARK:- 遵守UICollectionView的数据源协议
-extension RecommendViewController : UICollectionViewDataSource{
+// MARK:- 遵守UICollectionView的数据源协议， 遵守
+extension RecommendViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     //一共有多少组
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 12
@@ -97,11 +105,19 @@ extension RecommendViewController : UICollectionViewDataSource{
         return 4 //其余的情况都是只有4条记录
     }
     
-    //
+    //获取cell
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        //1 后去cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        //1 定义cell
+        let cell : UICollectionViewCell!  //collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
         
+        //2 按条件获取指定的cell
+        if(indexPath.section == 1) {
+            //获取颜值cell
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPrettyCellID, for: indexPath)
+        } else {
+            //获取普通cell
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        }
         return cell
     }
     
@@ -112,5 +128,14 @@ extension RecommendViewController : UICollectionViewDataSource{
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderViewID, for: indexPath)
         
         return headerView
+    }
+    
+    //针对协议UICollectionViewDelegateFlowLayout，实现item尺寸的设置
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if(indexPath.section == 1) {
+            return CGSize(width: kItemW, height: kPrettyItemH)
+        }
+        
+        return CGSize(width: kItemW, height: kNormalItemH)
     }
 }
---
+++ DYZB/DYZB/Classes/Main/View/CollectionPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionPrettyCell.swift
+//  DYZB
+//
+//  Created by CHANGJIAN ZHAO on 2018/9/17.
+//  Copyright © 2018年 hingin. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
